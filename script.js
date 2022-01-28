8f432a20cd2dd717ba390372ba4924ba69a9bd61
WarriorX623 <owenhwtam@gmail.com>
2022-01-28 18:43:51 +0800

    Trying to fix nav

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let scene = new ScrollMagic.Scene({
   .addTo(controller);
 const nav = document.querySelector("nav");
 const navButton = document.querySelector(".nav-button");
+nav.classList.remove("nav-active");
 navButton.addEventListener("click", function () {
   nav.classList.toggle("nav-active");
   navButton.classList.toggle("nav-button-active");
