32587f90ec7101a95d03ae63b6bdb4c5cb7c712a
WarriorX623 <owenhwtam@gmail.com>
2022-01-28 21:22:50 +0800

    Made sure all didnt include all

--- script.js
+++ script.js
@@ -15,8 +15,22 @@ let scene = new ScrollMagic.Scene({
   .addTo(controller);
 const nav = document.querySelector("nav");
 const navButton = document.querySelector(".nav-button");
+const navbar = document.querySelector(".navbar");
 nav.classList.remove("nav-active");
 navButton.addEventListener("click", function () {
   nav.classList.toggle("nav-active");
   navButton.classList.toggle("nav-button-active");
+});
+navbar.addEventListener("click", function (e) {
+  e.preventDefault();
+  nav.classList.toggle("nav-active");
+  navButton.classList.toggle("nav-button-active");
+  if (!e.target.classList.contains("nav-link")) {
+    return;
+  }
+  const scrollTo = document.querySelector(
+    `#section-${e.target.dataset.section}`
+  );
+  console.log(scrollTo);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
