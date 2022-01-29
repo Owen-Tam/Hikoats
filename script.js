4bca79f29a12d7a3938ed0cbd159f3e95d370564
WarriorX623 <owenhwtam@gmail.com>
2022-01-29 15:24:37 +0800

    Slightly longer oad

--- script.js
+++ script.js
@@ -38,5 +38,5 @@ const loader = document.querySelector(".loader");
 window.addEventListener("load", function () {
   this.setTimeout(function () {
     loader.classList.add("finish");
-  }, 2000);
+  }, 2200);
 });
